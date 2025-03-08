c413e9809dd957ab30ee149a9131fe81e9edea46
Ullyanne <ujfp@ic.ufal.br>
2025-03-08 14:40:13 -0300

    change rearrangeseq function

--- run.sh
+++ run.sh
@@ -5,10 +5,8 @@ make clean
 make
 cd ..
 
-dir="/home/ullyanne/Documents/2spp/instances/bke/T40/"
+dir="/home/ullyanne/Documents/2spp/instances/bke/T20"
 
 find "$dir" -type f -iname "*.txt" | while read file; do
-    for i in {1..20}; do
-        src/samplecode -f "$file" -k 2 -m 0.7 -e 0.3 -o 0.6 -p 60 -t 1 -b 100 -g 100 -d
-    done
+    src/samplecode -f "$file" -k 2 -m 0.7 -e 0.3 -o 0.6 -p 60 -t 1 -b 100 -g 100 -d
 done
